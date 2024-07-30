45c059918d78a90b112d308874fb884e040c97ad
daffa <daffazakaria19@gmail.com>
2024-07-30 19:19:28 +0700

    D:\SQA\java-fundamental

---
+++ src/main/java/com/juaracoding/MainApp.java
@@ -0,0 +1,18 @@
+package com.juaracoding;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Juara!");
+        int number = 10;
+        if(true){
+            System.out.println(number);
+            System.out.println("True");
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(number);
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/com/juaracoding/Variable.java
@@ -0,0 +1,8 @@
+package com.juaracoding;
+
+public class Variable {
+
+        public static void main(String[] args){
+
+        }
+}
