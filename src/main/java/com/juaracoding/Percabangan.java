df1b2ab5d36926fb43cc5595b1419a43f9b27ce3
kryzler9 <daffazakaria19@gmail.com>
2024-07-31 21:29:18 +0700

    D:\SQA\java-fundamental

---
+++ src/main/java/com/juaracoding/Percabangan.java
@@ -0,0 +1,42 @@
+package com.juaracoding;
+
+public class Percabangan {
+
+    public static void main(String[] args){
+
+        if(true){
+            System.out.println("Statement");
+        }
+
+        System.out.println("Next Statement");
+
+
+        // program ganjil genap (if else)
+        int platNomor = 1234;
+
+        if(platNomor % 2 == 0){ // 0 == 0 (false)
+            System.out.println("Plat Nomor Genap");
+        }
+        else{
+            System.out.println("Plat Nomor Ganjil");
+        }
+
+        // if majemuk
+        // size, price
+        String size = "L";
+        int price;
+        if(size == "S"){
+            price = 40000;
+        }else if(size == "M") {
+            price = 50000;
+        } else if(size == "L") {
+            price = 60000;
+        }
+        System.out.println("Size: "+size+"Price: "+price);
+
+        // nested if
+
+
+        }
+    }
+}
